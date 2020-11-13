77abc057f0d7a7da9258908732adefb29979c575
Nhan Vo <niick7@gmail.com>
2020-11-13 01:49:56 -0600

    Remove redundant code

--- src/main/java/company/Application.java
+++ src/main/java/company/Application.java
@@ -5,7 +5,7 @@ import java.time.LocalDate;
 public class Application {
   public static void main(String[] args) {
     System.out.println("Updating...");
-    Manager ceo = new Manager("001", "CEO", "A", LocalDate.of(1982, 12, 01));
+    Manager ceo = new Manager("001", "CEO", "A", LocalDate.of(1982, 12, 1));
 
     Employee e1 = new Employee("002", "Tom", "B", LocalDate.of(1984, 10, 28));
     Employee e2 = new Employee("003", "Peter", "C", LocalDate.of(1994, 8, 8));
@@ -13,9 +13,9 @@ public class Application {
     Employee e4 = new Employee("005", "Charles", "E", LocalDate.of(1996, 4, 22));
     Employee e5 = new Employee("006", "DJ", "F", LocalDate.of(1989, 3, 12));
     Employee e6 = new Employee("007", "Gratify", "G", LocalDate.of(1995, 5, 18));
-    Manager m1 = new Manager("008", "Rock", "H", LocalDate.of(1988, 7, 16));;
-    Manager m2 = new Manager("009", "Miller", "I", LocalDate.of(1989, 2, 15));;
-    Manager m3 = new Manager("010", "Marry", "J", LocalDate.of(1987, 1, 03));;
+    Manager m1 = new Manager("008", "Rock", "H", LocalDate.of(1988, 7, 16));
+    Manager m2 = new Manager("009", "Miller", "I", LocalDate.of(1989, 2, 15));
+    Manager m3 = new Manager("010", "Marry", "J", LocalDate.of(1987, 1, 3));
 
     m1.addEmployee(e1);
     m1.addEmployee(e2);
